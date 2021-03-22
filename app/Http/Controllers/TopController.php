5d30738d23e3a91838136ef8c858d801ed9acd2e
cuongdp <phucuongdo1996@gmail.com>
2021-03-22 17:24:11 +0700

    Build data base

--- app/Http/Controllers/TopController.php
+++ app/Http/Controllers/TopController.php
@@ -2,10 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\Hero\HeroRepositoryInterface;
 use Illuminate\Support\Facades\Auth;
 
 class TopController extends Controller
 {
+    /**
+     * @var \App\Repositories\Hero\HeroEloquentRepository
+     */
+    protected $heroRepository;
+
+    public function __construct(
+        HeroRepositoryInterface $heroRepository
+    ) {
+        $this->heroRepository = $heroRepository;
+    }
+
     /**
      * Show dota site
      *
@@ -28,6 +40,7 @@ class TopController extends Controller
 
     public function dotaListSet()
     {
-        return view('dota.list_set');
+        $listHero = $this->heroRepository->getListHero();
+        return view('dota.list_set', compact('listHero'));
     }
 }
---
+++ app/Repositories/Hero/HeroRepositoryInterface.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Repositories\Hero;
+
+interface HeroRepositoryInterface
+{
+}
