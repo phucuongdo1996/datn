df3fd25487e24f5bcf45b351bc59cb7ac12ae78b
cuongdp <phucuongdo1996@gmail.com>
2021-04-27 15:30:16 +0700

    Tao link url bao kim

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,48 +2,48 @@
     <div id="main">
         <div id="kvWrap" style="padding-left: 10%; padding-right: 10%;">
             <div class="row p20t">
-                <div class="col-4">
+                <div class="col-4 p15r">
                     <div class="item-block m10b m5r p15" style="height: 300px">
                         <div class="row m-0">
                             <div class="col-12 d-flex justify-content-center p20">
                                 <i class="far fa-thumbs-up fs50"></i>
                             </div>
                             <div class="col-12 m10b d-flex justify-content-center">
-                                <p class="fs16 fw-bold m-0">CHẤT LƯỢNG</p>
+                                <p class="fs20 fw-bold m-0">CHẤT LƯỢNG</p>
                             </div>
                             <div class="col-12">
-                                <p>Trang web cung cấp các item từ những nguồn uy tín để đảm bảo các item được bán là hợp lệ.</p>
+                                <p class="fs18">Trang web cung cấp các item từ những nguồn uy tín để đảm bảo các item được bán là hợp lệ.</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-4">
+                <div class="col-4 p5lr">
                     <div class="item-block m10b m5r p15" style="height: 300px">
                         <div class="row m-0">
                             <div class="col-12 d-flex justify-content-center p20">
                                 <i class="fas fa-check-circle fs50"></i>
                             </div>
                             <div class="col-12 m10b d-flex justify-content-center">
-                                <p class="fs16 fw-bold m-0">UY TÍN</p>
+                                <p class="fs20 fw-bold m-0">UY TÍN</p>
                             </div>
                             <div class="col-12">
-                                <p>Bạn có thể hoàn toàn yên tâm khi giao dịch tại đây.</p>
-                                <p>Trang web đã được cộng đồng Dota2Vn confirm. Được xác nhận từ các top Trader</p>
+                                <p class="fs18">Bạn có thể hoàn toàn yên tâm khi giao dịch tại đây.</p>
+                                <p class="fs18">Trang web đã được cộng đồng Dota2Vn confirm. Được xác nhận từ các top Trader</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-4">
+                <div class="col-4 p15l">
                     <div class="item-block m10b m5r p15" style="height: 300px">
                         <div class="row m-0">
                             <div class="col-12 d-flex justify-content-center p20">
                                 <i class="fas fa-project-diagram fs50"></i>
                             </div>
                             <div class="col-12 m10b d-flex justify-content-center">
-                                <p class="fs16 fw-bold m-0">ĐA DẠNG</p>
+                                <p class="fs20 fw-bold m-0">ĐA DẠNG</p>
                             </div>
                             <div class="col-12">
-                                <p>Các item được cập nhật hàng ngày với số lượng đa dạng</p>
+                                <p class="fs18">Các item được cập nhật hàng ngày với số lượng đa dạng</p>
                             </div>
                         </div>
                     </div>
